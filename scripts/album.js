0fc8f0ea7ec58f6f8eee9aa18f7cee2da3c1f145
Bachir Abi Habib <bashir.abihabib@gmail.com>
2016-11-06 20:39:39 -0600

    fixed my checkpoint-11-albumview

--- scripts/album.js
+++ scripts/album.js
@@ -1,5 +1,4 @@
-
- var albumPicasso = {
+var albumPicasso = {
      title: 'The Colors',
      artist: 'Pablo Picasso',
      label: 'Cubism',
@@ -13,7 +12,6 @@
          { title: 'Magenta', duration: '2:15'}
      ]
  };
- 
  
  var albumMarconi = {
      title: 'The Telephone',
@@ -42,24 +40,21 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
-var setCurrentAlbum = function(album) {
-    
+ var setCurrentAlbum = function(album) {
+  
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
      
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
- 
-    
+     
      albumSongList.innerHTML = '';
  
-    
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
